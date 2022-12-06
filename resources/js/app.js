657fc482f4bdddbbf47044b85b1a86c785987eba
NightingaleWK <33059842+NightingaleWK@users.noreply.github.com>
2022-12-06 16:52:57 +0800

    翻译 URL 标示

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,3 +2,10 @@ import './bootstrap';
 
 import '../sass/app.scss';
 import * as bootstrap from 'bootstrap';
+
+try {
+    // 加载 jQuery
+    window.$ = window.jQuery = require('jquery');
+  
+    require('bootstrap');
+  } catch (e) {}
--- routes/web.php
+++ routes/web.php
@@ -29,7 +29,8 @@ Auth::routes(['verify' => true]);
 Route::resource('users', UsersController::class, ['only' => ['show', 'update', 'edit']]);
 
 // 话题相关
-Route::resource('topics', TopicsController::class, ['only' => ['index', 'show', 'create', 'store', 'update', 'edit', 'destroy']]);
+Route::resource('topics', TopicsController::class, ['only' => ['index', 'create', 'store', 'update', 'edit', 'destroy']]);
+Route::get('topics/{topic}/{slug?}', [TopicsController::class, 'show'])->name('topics.show');
 
 // 分类相关
 Route::resource('categories', CategoriesController::class, ['only' => ['show']]);
