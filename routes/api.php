edfb6474fc7bd23551c59a9a1d137b857beec1a8
NightingaleWK <djsywxsj@outlook.com>
2023-10-12 17:46:31 +0800

    暂存短信验证代码

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Api\VerificationCodesController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -15,4 +16,6 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::prefix('v1')->name('api.v1.')->group(function () {
+    // 短信验证码
+    Route::post('verificationCodes', [VerificationCodesController::class, 'store'])->name('verificationCodes.store');
 });
