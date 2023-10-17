c2eceadeb99a97fc9685c53964b58b929e8d1551
NightingaleWK <djsywxsj@outlook.com>
2023-10-17 16:32:10 +0800

    消息通知统计

--- routes/api.php
+++ routes/api.php
@@ -117,6 +117,10 @@ Route::prefix('v1')
                     Route::apiResource('notifications', NotificationsController::class)->only([
                         'index'
                     ]);
+
+                    // 通知统计
+                    Route::get('notifications/stats', [NotificationsController::class, 'stats'])
+                        ->name('notifications.stats');
                 });
             });
     });
