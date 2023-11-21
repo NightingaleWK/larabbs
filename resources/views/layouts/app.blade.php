1a1b3de946c61ddc15c402c65fba2fee8d194b48
NightingaleWK <djsywxsj@outlook.com>
2023-11-21 16:30:49 +0800

    样式调整

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -11,23 +11,21 @@
 
     <title>@yield('title', 'LaraBBS') - LaraBBS 进阶教程</title>
 
-    {{-- Style --}}
-    <link rel="stylesheet" href="{{ mix('css/app.css') }}">
+    {{-- vite --}}
+    @vite(['resources/js/app.js'])
 </head>
 
 <body>
     <div id='app' class="{{ route_class() }}-page">
         @include('layouts._header')
         <div class="container">
-            @include('shared._message')
+            @include('shared._messages')
 
             @yield('content')
         </div>
         @include('layouts._footer')
     </div>
 
-    {{-- Scripts --}}
-    <script src="{{ mix('js/app.js') }}"></script>
 </body>
 
 </html>
