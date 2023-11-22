c9dea24544d5bc3631698a62d8200c22ba008ee0
NightingaleWK <djsywxsj@outlook.com>
2023-11-22 17:19:30 +0800

    用户个人页面原型

---
+++ app/Http/Controllers/UsersController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+
+class UsersController extends Controller
+{
+    /**
+     * Display the specified resource.
+     */
+    public function show(User $user)
+    {
+        return view('users.show', compact('user'));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\PagesController;
+use App\Http\Controllers\UsersController;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
@@ -19,3 +20,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', [PagesController::class, 'root'])->name('root');
 
 Auth::routes(['verify' => true]);
+
+Route::resource('users', UsersController::class)->only([
+    'show', 'edit', 'update'
+]);
