63d4248a3994d18025ff075d5f42ebdbd6485aa1
NightingaleWK <djsywxsj@outlook.com>
2023-11-22 16:18:33 +0800

    认证后的消息提醒

---
+++ app/Listeners/EmailVerified.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Listeners;
+
+use Illuminate\Auth\Events\Verified;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Queue\InteractsWithQueue;
+
+class EmailVerified
+{
+    /**
+     * Create the event listener.
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Handle the event.
+     */
+    public function handle(Verified $event): void
+    {
+        // 会话里闪存认证成功后的消息提醒
+        session()->flash('success', '邮箱验证成功 ^_^');
+    }
+}
--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -2,8 +2,11 @@
 
 namespace App\Providers;
 
+use App\Listeners\EmailVerified;
 use Illuminate\Auth\Events\Registered;
+use Illuminate\Auth\Events\Verified;
 use Illuminate\Auth\Listeners\SendEmailVerificationNotification;
+use Illuminate\Auth\Middleware\EnsureEmailIsVerified;
 use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
 use Illuminate\Support\Facades\Event;
 
@@ -17,6 +20,9 @@ class EventServiceProvider extends ServiceProvider
     protected $listen = [
         Registered::class => [
             SendEmailVerificationNotification::class,
+        ],
+        Verified::class => [
+            EmailVerified::class,
         ],
     ];
 
