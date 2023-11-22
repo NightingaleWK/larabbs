b14beaa6fb8a8d598757714cf506f5dbd52b7443
NightingaleWK <djsywxsj@outlook.com>
2023-11-22 14:25:59 +0800

    生成用户认证代码

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
+use App\Http\Controllers\HomeController;
 use App\Http\Controllers\PagesController;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,3 +17,5 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', [PagesController::class, 'root'])->name('root');
+
+Auth::routes();
