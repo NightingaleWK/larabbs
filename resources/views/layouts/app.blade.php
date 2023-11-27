566c5e8a3bb8ff1b3cbac9765e7d3fcfb61bfd41
NightingaleWK <djsywxsj@outlook.com>
2023-11-27 16:56:08 +0800

    添加 wangeditor 插件

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -13,6 +13,8 @@
 
     {{-- vite --}}
     @vite(['resources/js/app.js'])
+
+    <link href="https://unpkg.com/@wangeditor/editor@latest/dist/css/style.css" rel="stylesheet">
 </head>
 
 <body>
@@ -26,6 +28,38 @@
         @include('layouts._footer')
     </div>
 
+    <script src="https://unpkg.com/@wangeditor/editor@latest/dist/index.js"></script>
+    <script>
+        const {
+            createEditor,
+            createToolbar
+        } = window.wangEditor
+
+        const editorConfig = {
+            placeholder: 'Type here...',
+            onChange(editor) {
+                const html = editor.getHtml()
+                console.log('editor content', html)
+                // 也可以同步到 <textarea>
+            }
+        }
+
+        const editor = createEditor({
+            selector: '#editor-container',
+            html: '<p><br></p>',
+            config: editorConfig,
+            mode: 'default', // or 'simple'
+        })
+
+        const toolbarConfig = {}
+
+        const toolbar = createToolbar({
+            editor,
+            selector: '#toolbar-container',
+            config: toolbarConfig,
+            mode: 'default', // or 'simple'
+        })
+    </script>
 </body>
 
 </html>
