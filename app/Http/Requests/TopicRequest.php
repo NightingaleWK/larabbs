f9169c1c14da49ff3e9b0f4fd3dc0f0e07c165ac
NightingaleWK <djsywxsj@outlook.com>
2023-11-27 16:41:14 +0800

    用户可以创建话题

--- app/Http/Requests/TopicRequest.php
+++ app/Http/Requests/TopicRequest.php
@@ -6,36 +6,29 @@ class TopicRequest extends Request
 {
     public function rules()
     {
-        switch($this->method())
-        {
-            // CREATE
+        switch ($this->method()) {
             case 'POST':
-            {
-                return [
-                    // CREATE ROLES
-                ];
-            }
-            // UPDATE
             case 'PUT':
-            case 'PATCH':
-            {
-                return [
-                    // UPDATE ROLES
-                ];
-            }
+            case 'PATCH': {
+                    return [
+                        'title' => 'required|min:2',
+                        'body' => 'required|min:3',
+                        'category_id' => 'required|numeric',
+                    ];
+                }
             case 'GET':
             case 'DELETE':
-            default:
-            {
-                return [];
-            }
+            default: {
+                    return [];
+                }
         }
     }
 
     public function messages()
     {
         return [
-            // Validation messages
+            'title.min' => '标题 必须至少两个字符',
+            'body.min' => '文章内容 必须至少三个字符',
         ];
     }
 }
