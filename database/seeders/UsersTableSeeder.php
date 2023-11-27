3cf660608caeab8518bcb9e924f55f63f559a4b2
NightingaleWK <djsywxsj@outlook.com>
2023-11-27 14:29:58 +0800

    填充用户和话题数据

---
+++ database/seeders/UsersTableSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        // 生成数据集合
+        User::factory()->count(10)->create();
+
+        // 单独处理第一个用户的数据
+        $user = User::find(1);
+        $user->name = 'NightingaleWK';
+        $user->email = 'ytzywk@163.com';
+        $user->avatar = 'https://cdn.learnku.com/uploads/images/201710/14/1/ZqM7iaP4CR.png';
+        $user->save();
+    }
+}
