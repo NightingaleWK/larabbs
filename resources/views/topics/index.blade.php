c7b0f67755b04537af998c0bae08d14ffbf630b5
NightingaleWK <djsywxsj@outlook.com>
2023-11-27 16:10:10 +0800

    话题排序

--- resources/views/topics/index.blade.php
+++ resources/views/topics/index.blade.php
@@ -17,8 +17,19 @@
 
                 <div class="card-header bg-transparent">
                     <ul class="nav nav-pills">
-                        <li class="nav-item"><a class="nav-link active" href="#">最后回复</a></li>
-                        <li class="nav-item"><a class="nav-link" href="#">最新发布</a></li>
+
+                        <li class="nav-item">
+                            <a class="nav-link @if (env('APP_URL') . request()->getRequestUri() !== request()->url() . '?order=recent') active @endif"
+                                href="{{ Request::url() }}?order=default">
+                                最后回复
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link @if (env('APP_URL') . request()->getRequestUri() == request()->url() . '?order=recent') active @endif"
+                                href="{{ Request::url() }}?order=recent">
+                                最新发布
+                            </a>
+                        </li>
                     </ul>
                 </div>
 
