abc87a4cb6e0d8941cb615a9d81a45337a9b59d4
NightingaleWK <djsywxsj@outlook.com>
2023-11-28 09:13:51 +0800

    修复 XSS 注入漏洞

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use App\Models\Topic;
+use App\Observers\TopicObserver;
 use Illuminate\Pagination\Paginator;
 use Illuminate\Support\ServiceProvider;
 
@@ -21,5 +23,6 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         Paginator::useBootstrap();
+        Topic::observe(TopicObserver::class);
     }
 }
